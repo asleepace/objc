6d404064b7599e43a1c5616906aa2d243a1c6c79
Colin Teahan <colin_teahan@yahoo.com>
2021-07-10 02:30:50 -0700

    adding the super reduced string

---
+++ Examples/Snippets/SuperReducedString/SuperReducedString.m
@@ -0,0 +1,17 @@
+//
+//  SuperReducedString.m
+//  Examples
+//
+//  Created by padlet on 7/10/21.
+//
+
+#import "SuperReducedString.h"
+
+@implementation SuperReducedString
+
+- (NSString *) superReducedString:(NSString *) {
+  
+  return s;
+}
+
+@end
