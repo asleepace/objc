3db3bd66d5e1ba1235673876bc88e0fbfbc24614
Colin Teahan <colin_teahan@yahoo.com>
2021-07-10 02:40:30 -0700

    adding the reduced string

--- Examples/Snippets/SuperReducedString/SuperReducedString.m
+++ Examples/Snippets/SuperReducedString/SuperReducedString.m
@@ -9,9 +9,23 @@
 
 @implementation SuperReducedString
 
-- (NSString *) superReducedString:(NSString *) {
+- (NSString *)superReducedString:(NSString *)s {
   
-  return s;
+  if (s == nil) return @"";
+  
+  NSString *reducedString = [self superReducedString:s];
+  
+  for (NSUInteger i=1; i<reducedString.length; i++) {
+    const char c1 = [reducedString characterAtIndex:i];
+    const char c2 = [reducedString characterAtIndex:i-1];
+    if (c1 == c2) {
+      NSString *prevStr = [reducedString substringToIndex:i-1];
+      NSString *nextStr = [reducedString substringFromIndex:i];
+      return [NSString stringWithFormat:@"%@%@", prevStr, nextStr];
+    }
+  }
+  
+  return reducedString;
 }
 
 @end
