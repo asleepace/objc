4a558de54799354d99dc40205aab8b937af34fdf
Colin Teahan <colin_teahan@yahoo.com>
2021-07-10 02:20:37 -0700

    removing old files

--- timeConversion.m
+++
@@ -1,9 +0,0 @@
-NSString * timeConversion(NSString *s)
-{
-  NSDateFormatter *format = [NSDateFormatter new];
-  [format setDateFormat:@"hh:mm:ssa"];
-  NSDate *date = [format dateFromString:s];
-  NSLog(@"date: %@",[date description]);
-  [format setDateFormat:@"HH:mm:ss"];
-  return [format stringFromDate:date];
-}
